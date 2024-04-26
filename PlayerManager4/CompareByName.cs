8595b458718d5deff14ac919017340922db2daa8
Daniel Silva <danielhull.silva1@gmail.com>
2024-04-26 17:19:02 +0000

    Exercicio 6 realizado

---
+++ PlayerManager4/CompareByName.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace PlayerManager4
+{
+    public class CompareByName : IComparer<Player>
+    {
+        bool _ascendOrder;
+
+        public CompareByName(bool _ascendOrder)
+        {
+            this._ascendOrder = _ascendOrder;
+        }
+        public int Compare(Player x, Player y)
+        {
+            int result = string.Compare(x.Name, y.Name);
+            return _ascendOrder ? -result : result;
+        }
+    }
+}
